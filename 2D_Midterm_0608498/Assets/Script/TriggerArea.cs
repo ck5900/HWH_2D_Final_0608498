89abfb54aeb691b1eeb59e44fc2ab3fc70e9b94d
ck5900 <71476403+ck5900@users.noreply.github.com>
2021-04-23 12:31:18 +0800

    發布成網頁版

---
+++ 2D_Midterm_0608498/Assets/Script/TriggerArea.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    //陣列 在類型後面加上中括號
+    //用於保存相同類型的複數資料
+    [Header("要關閉的怪物們")]
+    public GameObject[] monsters;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "球球")
+            monsters[0].SetActive(false);
+    }
+}
