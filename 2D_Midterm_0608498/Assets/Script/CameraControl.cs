981c536cd07f2352f8442f69b7fabd9eb4195b3d
ck5900 <71476403+ck5900@users.noreply.github.com>
2021-04-23 11:52:40 +0800

    發布成網頁版

---
+++ 2D_Midterm_0608498/Assets/Script/CameraControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("追蹤速度"), Range(0, 50)]
+    public float speed = 1.5f;
+    [Header("上下邊界")]
+    public Vector2 limitＹ = new Vector2(-5, 5);
+    [Header("左右邊界")]
+    public Vector2 limitＸ = new Vector2(-5, 5);
+    [Header("玩家")]
+    public Transform player;
+
+    private void Update()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Vector3 posCam = transform.position;   
+        Vector3 posPla = player.position;     
+
+        posCam = Vector3.Lerp(posCam, posPla, 0.5f * speed * Time.deltaTime);
+        posCam.z = -10;
+
+
+
+        posCam.x = Mathf.Clamp(posCam.x, limitＸ.x, limitＸ.y);
+        posCam.y = Mathf.Clamp(posCam.y, limitＹ.x, limitＹ.y);
+
+        transform.position = posCam;         
+
+    }
+
+}
+
---
+++ 2D_Midterm_0608498/Assets/Script/Player.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    //修飾詞 類型 名稱(指定值);
+
+
+    //類型 四大類型
+    //整數 int
+    //浮點數 float
+    //布林值 bool true是、false否
+    //字串 string
+    [Header("等級")]
+    [Tooltip("這是角色的等級")]
+    public int Lv = 1;
+    [Header("移動速度"), Range(0, 300)]
+    public float speed = 10.5f;
+    [Header("角色是否死亡")]
+    public bool isDead = false;
+    [Header("角色名稱"), Tooltip("這是角色的名稱")]
+    public string cName = "玩家";
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("變形元件")]
+    public Transform tra;
+    [Header("動畫元件")]
+    public Animator ani;
+    [Header("偵測範圍")]
+    public float rangeAttack = 2.5f;
+
+    //事件：繪製圖示
+    private void OnDrawGizmos()
+    {
+        //指定圖示顏色(紅,綠,藍,透明)
+        Gizmos.color = new Color(0, 1, 1, 0.3f);
+        //繪製圖示 球體(中心點,半徑)
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
+
+    //方法語法 Method-儲存複雜的程式區塊或演算法
+    //修飾詞 類型 名稱(){程式區塊或演算法}
+    //void無類型
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+
+    private void Move()
+    {
+        print("移動");
+
+        float h = joystick.Horizontal;
+        //float v = joystick.Vertical;
+
+        tra.Translate(h * speed, 0, 0);
+        ani.SetFloat("水平", h);
+        //ani.SetFloat("垂直", v);
+
+
+    }
+
+    public void Attack()
+    {
+
+        print("攻擊");
+
+        //2D物理 圓形碰撞(中心點,半徑,方向,距離,圖層編號)
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, rangeAttack, -transform.up, 0, 1 << 8);
+
+        if (hit && hit.collider.tag == "道具") hit.collider.GetComponent<Item>().DropProp();
+
+    }
+    private void Hit()
+    {
+
+    }
+    private void Dead()
+    {
+
+    }
+
+    //事件-特定時間會執行的方法
+    //開始事件：撥放後執行一次
+    private void Start()
+    {
+        //呼叫方法
+        //方法名稱();
+
+    }
+    //更新事件：大約一秒執行六十次 60FPS
+    private void Update()
+    {
+        Move();
+
+    }
+
+    [Header("金幣數量")]
+    public Text textCoin;
+
+    private int coin;
+
+    //觸發事件-進入:兩個物件必須有一個勾選 Is Trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "金幣")
+        {
+            coin++;
+            Destroy(collision.gameObject);
+            textCoin.text = "金幣:" + coin;
+        }
+    }
+}
+
